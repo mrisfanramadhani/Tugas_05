05a3446a4b59f8a7c893b909168c1351e150899d
mrisfanramadhani <32300986+mrisfanramadhani@users.noreply.github.com>
2017-11-08 23:18:30 +0800

    Penyelesaian

--- Tugas_05.cpp
+++ Tugas_05.cpp
@@ -7,5 +7,16 @@ int main(){
 	cout<<"======================"<<endl;
 	cout<<"Masukkan Tinggi Badan = ";cin>>tb;
 	cout<<"Masukkan Berat Badan = ";cin>>bb;
-	
+	if(bb<(tb/2.5)){
+		cout<<"Anda Termasuk Kurus (UNDERWEIGHT)";
+	}
+	else if(tb/2.5<=bb<=tb/2.3){
+		cout<<"Anda Termasuk Berat Badan Normal";
+	}
+	else if((tb/2.3)<bb){
+		cout<<"Anda Termasuk Gemuk (OVERWEIGHT)";
+	}
+	else{
+		cout<<"INPUT TIDAK VALID!!!!!!";
+	}	
 }

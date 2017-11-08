baa1fb29af2abd9fcb8c43c64109d899ed222226
mrisfanramadhani <32300986+mrisfanramadhani@users.noreply.github.com>
2017-11-08 23:15:25 +0800

    Input Tinggi Bedan

--- Tugas_05.cpp
+++ Tugas_05.cpp
@@ -1,4 +1,10 @@
 #include <iostream>
 using namespace std;
 int main(){
+	int tb,bb,hasil, hasil2;
+	cout<<"======================"<<endl;
+	cout<<"MENGHITUNG BADAN IDEAL"<<endl;
+	cout<<"======================"<<endl;
+	cout<<"Masukkan Tinggi Badan = ";cin>>tb;
+
 }
